b98890463e9bde1894c92501345fbbd9a2001241
sujitacharay2005 <sujitacharya2005@gmail.com>
2018-08-22 16:37:52 +0530

    19. inline template multiline  use  tactics

--- FirstAngularApp/src/app/servers/servers.component.ts
+++ FirstAngularApp/src/app/servers/servers.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  // inline template single quote one line use tactics  for multiline `
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
