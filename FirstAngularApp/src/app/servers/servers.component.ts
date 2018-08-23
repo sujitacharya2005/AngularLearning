a8aa4476bc44c452c1768d53bea887f09605b5ff
sujitacharay2005 <sujitacharya2005@gmail.com>
2018-08-23 22:54:41 +0530

    *ngIf directives

--- FirstAngularApp/src/app/servers/servers.component.ts
+++ FirstAngularApp/src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Test Server';
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
   onUpdateServerName(event: Event) {
